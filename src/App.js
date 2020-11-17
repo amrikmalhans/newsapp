2474a3c0c4a23f60e7f6b57fd36c7a64e160180f
am-coding <amrikdeveloper@gmail.com>
2020-11-17 18:54:59 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import SearchBox from './components/searchBox';
 import Navbar from './components/Navbar';
+import Headline from './components/Headline';
+import { Switch, Route } from "react-router-dom";
+import {getNews} from './services/headlines';
 
 function App() {
+  const [data, setData] = useState([]);
+
+    useEffect(() => {
+        let mounted = true;
+        getNews()
+            .then(articles => {
+                if(mounted) {
+                    setData(articles)
+                }
+            })
+            return () => mounted = false;
+    }, [])
+
+
   return (
     <div className="App">
-     <Navbar />
-     <SearchBox />
+      <Switch>
+        <Route exact path='/'> 
+          <Navbar/>
+          <SearchBox data={data} />
+        </Route>
+        <Route exact path='/mao' component={Headline} />
+     </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
